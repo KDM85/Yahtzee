b91a0fa0d813f956e77adc3fe753ca8a7d98b08f
KDM85 <102189332+KDM85@users.noreply.github.com>
2023-07-24 11:46:34 -0400

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,33 @@
+TITLE = "Yahtzee!"
+WIDTH = 480
+HEIGHT = 800
+LABEL_FONT = ("Tahoma", 16)
+BUTTON_FONT = ("Tahoma", 16, "bold")
+DICE_FONT = ("Tahoma", 24, "bold")
+
+HAND = [0, 0, 0, 0, 0]
+SELECTED_DICE = ["", "", "", "", ""]
+SELECTED_OPTION = ""
+
+OPTIONS = {
+    "aces": "Aces",
+    "twos": "Twos",
+    "threes": "Threes",
+    "fours": "Fours",
+    "fives": "Fives",
+    "sixes": "Sixes",
+    "upper_score": "Upper Score",
+    "upper_bonus": "Bonus (if >= 63)",
+    "upper_total": "Upper Total",
+    "three_of_a_kind": "Three of a Kind",
+    "four_of_a_kind": "Four of a Kind",
+    "full_house": "Full House (+25)",
+    "small_straight": "Small Straight (+30)",
+    "large_straight": "Large Straight (+40)",
+    "yahtzee": "Yahtzee! (+50)",
+    "chance": "Chance",
+    "yahtzee_bonus": "Yahtzee Bonus",
+    "lower_total": "Lower Total",
+    "upper_total2": "Upper Total",
+    "total_score": "Total Score",
+}
